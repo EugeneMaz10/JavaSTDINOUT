a02f4854abe02f4bedecd33a5b6eb7cac96830d5
EugeneMaz10 <46795401+EugeneMaz10@users.noreply.github.com>
2021-05-12 06:03:01 +0300

    Update io.java

--- io.java
+++ io.java
@@ -1,1 +1,20 @@
+#######################################HACKERANK##############################################
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+         int b = scan.nextInt();
+          int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+              System.out.println(b);
+                    System.out.println(c);
+                    
+        
+    }
+}
